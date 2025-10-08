f0f0421e81e9576c87aca6891f4e0603985a143c
sureshjoshi88 <sj3795789@gmail.com>
2025-10-08 10:52:41 +0530

    change title and add hover in foter

--- src/component/foter/Foter.jsx
+++ src/component/foter/Foter.jsx
@@ -9,7 +9,7 @@ const Foter = () => {
                     <p className='text-gray-300 pt-2'>Bringing hospital-level care to the comfort of your home with professional healthcare services.</p>
                 </div>
                 <div className='p-3 text-center'>
-                    <p className='pt-6 text-2xl text-white font-semibold cursor-pointer'>Services</p>
+                    <p className='pt-6 text-2xl text-white font-semibold cursor-pointer hover:underline'>Services</p>
                     <p className='text-gray-300 pt-2  cursor-pointer'>Nursing Care</p>
                     <p className='text-gray-300 pt-2  cursor-pointer'>ICU Setup</p>
                     <p className='text-gray-300 pt-2  cursor-pointer'>GDA Care</p>
@@ -18,12 +18,12 @@ const Foter = () => {
                     <p className='text-gray-300 pt-2  cursor-pointer'>Trained Attendant at home</p>
                 </div>
                 <div className='p-3 text-center'>
-                    <p className='pt-6 text-2xl text-white font-semibold cursor-pointer'>Support</p>
+                    <p className='pt-6 text-2xl text-white font-semibold cursor-pointer hover:underline'>Support</p>
                     <p className='text-gray-300 pt-2 cursor-pointer'>24/7 Emergency</p>
                     <p className='text-gray-300 pt-2 cursor-pointer'>Contact Us</p>
                 </div>
                 <div className='p-3 text-center'>
-                    <p className='pt-6 text-2xl text-white font-semibold cursor-pointer'>Contact</p>
+                    <p className='pt-6 text-2xl text-white font-semibold cursor-pointer hover:underline'>Contact</p>
                     <p className='text-gray-300 pt-2 cursor-pointer'>1-800-HOME-ICU</p>
                     <p className='text-gray-300 pt-2 cursor-pointer'>care@hospitalathome.com</p>
                     <p className='text-gray-300 pt-2 cursor-pointer'>72067-83874</p>
